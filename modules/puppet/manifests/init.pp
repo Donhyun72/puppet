35734daedf6f4c784bc128f2525e429907cef672
Donhyun Lee <donhyun.lee@cdnetworks.co.kr>
2016-09-08 08:19:04 +0000

    ssl

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -3,4 +3,30 @@ class puppet {
 		source => 'puppet:///modules/puppet/papply.sh',
 		mode	=> '0755',
 	}
+
+	file { '/usr/local/bin/pull-updates':
+		source => 'puppet:///modules/puppet/pull-updates.sh',
+		mode	=> '0755',
+	}
+
+	file { '/root/.ssh/id_rsa':
+		source => 'puppet:///modules/puppet/ubuntu.priv',
+		owner	=> 'root',
+		mode	=> '0600',
+		require => File["/root/.ssh"],
+	}
+
+	file { '/root/.ssh':
+		ensure	=> directory,
+		owner	=> 'root',
+		mode	=> '0700',
+	}
+
+	cron { 'run-puppet':
+		ensure	=> present,
+		user	=> 'root',
+		command	=> '/usr/local/bin/pull-updates',
+		minute	=> '*/10',
+		hour	=> '*',	
+	}
 }
