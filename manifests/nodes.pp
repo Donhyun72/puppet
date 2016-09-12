1ae652a2c5b298d2058be8c88631f2e5921a3506
Donhyun Lee <donhyun.lee@cdnetworks.co.kr>
2016-09-12 08:02:37 +0000

    aa

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,8 +1,9 @@
 node 'cookbook', 'cookbook2', 'cookbook-test' {
-	include puppet
+  include puppet
+  include memcached
 
-	file { '/tmp/hello2':
-		content	=> "Hello, world\n",
-	}
+  file { '/tmp/hello2':
+    content  => "Hello, world\n",
+  }
 
 }
--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,32 +1,34 @@
+
+# This is a test.
 class puppet {
-	file { '/usr/local/bin/papply':
-		source => 'puppet:///modules/puppet/papply.sh',
-		mode	=> '0755',
-	}
+  file { '/usr/local/bin/papply':
+    source  => 'puppet:///modules/puppet/papply.sh',
+    mode    => '0755',
+  }
 
-	file { '/usr/local/bin/pull-updates':
-		source => 'puppet:///modules/puppet/pull-updates.sh',
-		mode	=> '0755',
-	}
+  file { '/usr/local/bin/pull-updates':
+    source  => 'puppet:///modules/puppet/pull-updates.sh',
+    mode    => '0755',
+  }
 
-	file { '/root/.ssh/id_rsa':
-		source => 'puppet:///modules/puppet/ubuntu.priv',
-		owner	=> 'root',
-		mode	=> '0600',
-		require => File["/root/.ssh"],
-	}
+  file { '/root/.ssh/id_rsa':
+    source  => 'puppet:///modules/puppet/ubuntu.priv',
+    owner   => 'root',
+    mode    => '0600',
+    require => File['/root/.ssh'],
+  }
 
-	file { '/root/.ssh':
-		ensure	=> directory,
-		owner	=> 'root',
-		mode	=> '0700',
-	}
+  file { '/root/.ssh':
+    ensure  => directory,
+    owner   => 'root',
+    mode    => '0700',
+  }
 
-	cron { 'run-puppet':
-		ensure	=> present,
-		user	=> 'root',
-		command	=> '/usr/local/bin/pull-updates',
-		minute	=> '*/10',
-		hour	=> '*',	
-	}
+  cron { 'run-puppet':
+    ensure  => present,
+    user    => 'root',
+    command => '/usr/local/bin/pull-updates',
+    minute  => '*/10',
+    hour    => '*',
+  }
 }
